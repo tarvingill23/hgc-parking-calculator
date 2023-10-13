890d5a14d460e94085f01638b70f3e0d8d289aa4
Tarvin Gill <tarvingill@gmail.com>
2023-10-13 19:33:55 +1100

    updated ui

--- src/components/TimeInput.vue
+++ src/components/TimeInput.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
+    <h1>Date</h1>
+    <input type="date" placeholder="Date" v-model="ticketDate" />
+    {{ date }} {{ ticketDate }}
+  </div>
+  <div>
     <h1>Time</h1>
     <input type="time" placeholder="Insert time here" v-model="time" />
+    {{ time }}
   </div>
 
   <div>
-    <h1>Date</h1>
-    <p>{{ date }}</p>
+    <button @click="calculatePrice()">Calculate</button>
+    <h2>{{ result }}</h2>
   </div>
 </template>
 
@@ -14,8 +20,20 @@
 export default {
   data() {
     return {
-      date: new Date(),
+      dateObj: new Date(),
+      ticketTime: 0,
+      result: "",
+      unixTimeStamp: 0,
+      fullDateTime: "",
     };
+  },
+  methods: {
+    convertToUnix() {
+      this.fullDateTime = ``;
+    },
+    calculatePrice() {
+      this.result = "Hello";
+    },
   },
 };
 </script>
